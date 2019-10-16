4f2d4c0a1058d739e63b6871fbae9076ee3bc4de
Guilherme Bayer <guiilherme.bayer@gmail.com>
2019-10-16 14:47:49 -0300

    [ch627] Adicionando componente Tag

---
+++ src/components/Tag/index.js
@@ -0,0 +1,34 @@
+import React, { memo } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { theme } from 'styled-tools'
+
+const Content = styled.div`
+  background-color: ${({ theme, color }) => {
+    console.log(color)
+    return theme.colors[color].fifty
+  }};
+  border-radius: ${theme('border.radius.four')};
+  padding: 2px 6px;
+`
+
+export const Tag = memo(({ color, children }) => (
+  <Content color={color}>{children}</Content>
+))
+
+Tag.defaultProps = {
+  color: 'blue'
+}
+
+Tag.propTypes = {
+  color: PropTypes.oneOf([
+    'blue',
+    'gray',
+    'green',
+    'red',
+    'yellow',
+    'ciano',
+    'pink',
+    'orange'
+  ])
+}
